4e4618e6b1ef81e2ba2a59297b6f9f51a9e42aae
jira-eim <yousuke.hirano@jp.ricoh.com>
2019-01-29 09:40:17 +0900

    add in link.

--- snark/synnetdb.py
+++ snark/synnetdb.py
@@ -26,9 +26,19 @@ class SynNetDb:
     """
     wordnetを簡単に処理するための一時記憶
     RDF DBも併せて処理する
+
+    word in synset . 同義語, 個物化
+    synset isa synset . 抽象化, 属性
+    synset hasa synset . 部分/属性/材質/所有(目的語を抱えている状態の主語)
+    synset then synset . 状態遷移(主語から主語へ移る), 条件つきの場合は条件の付いたsynsetができる
     """
 
     startdict = None
+
+    v = False
+
+    def __init__(self, opts=''):
+        self.v = 'v' in opts
 
     def load_file(self, path):
         self.startdict = pd.read_csv(path)
@@ -37,12 +47,11 @@ class SynNetDb:
     def save_file(self, path):
         self.startdict.to_csv(path)
 
-    def load_isa_words_from_db(self, path, key):
+    def load_same_words_from_db(self, path, key):
         wn = wordnetdb.WordNetDb(path)
-
         cur = wn.get_same_words_by_lemma(key)
         for c in cur:
-            self.add_link(c[3], 'isa', c[1])
+            self.add_link(c[3], 'in', c[1])
 
     def select(self, key):
         d = self.startdict
@@ -65,27 +74,44 @@ class SynNetDb:
             l.append(r[0])
         return l
 
-    def select_eq(self, key):
-        d = self.startdict
-        link = 'eq'
-        res = d[((d['synset1'] == key) | (d['synset2'] == key)) & (d['link'] == link)]
-        l = []
-        for r in res.values:
-            l.append(r[0])
-            l.append(r[2])
-        return list(set(l))
+    def select_same(self, key):
+        """
+        keyと同じinを持つ全synsetを取得する
+        Returns
+        -------
+        [(synset, [word, ...]), ...]
+        """
+        isa = self.select_link(key, 'in')
+        res = []
+        for i in isa:
+            ref = self.select_link_ref(i, 'in')
+            res.append((i, ref))
+        return res
+
+    # def select_same(self, key):
+    #     """
+    #     同じ意味を持つsynsetを取得する
+    #     (同じisaを持っていても違う意味になることがあるのでsimilarとは異なる)
+    #     """
+    #     d = self.startdict
+    #     link = 'same'
+    #     res = d[((d['synset1'] == key) | (d['synset2'] == key)) & (d['link'] == link)]
+    #     l = []
+    #     for r in res.values:
+    #         l.append(r[0])
+    #         l.append(r[2])
+    #     return list(set(l))
 
     def add_link(self, synset1, link, synset2):
         tmp_se = pd.Series([synset1, link, synset2], index=['synset1', 'link', 'synset2'])
         self.startdict = self.startdict.append(tmp_se, ignore_index=True)
 
 # wordnetdbからデータをロードする
-rn = SynNetDb()
+rn = SynNetDb(opts='v')
 rn.load_file('dict/rn.csv')
-print(rn.select('d'))
 print(rn.select_link('犬', 'isa'))
 
-rn.load_isa_words_from_db('db/wnjpn.db', '犬')
-print(rn.select_eq('犬'))
+rn.load_same_words_from_db('db/wnjpn.db', '犬')
+print(rn.select_same('犬'))
 # rn.add_link('ネコ', 'isa', '動物')
 # print(rn.select_link_ref('動物', 'isa'))
